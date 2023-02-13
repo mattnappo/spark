4f483d815c499d9d6fe07682ee37f042f7de48a7
Matt Nappo <mnappo@u.rochester.edu>
2023-02-12 22:37:27 -0500

    Writing server

--- src/core/server.rs
+++ src/core/server.rs
@@ -1,1 +1,22 @@
-pub struct Server {}
+use std::path::Path;
+
+/// A running server instance
+pub struct Server {
+    /// A database mapping from `Header`s to `EncSecret`s
+    store: sled::Db,
+
+    /// The RPC server instance
+    rpc: Option<()>,
+
+    /// The server's key (in memory)
+    key: ServerKey,
+}
+
+impl Server {
+    /// Initialize a new server given path to key and database
+    fn load(key: Path, store: Path) {}
+
+    fn insert_secret(&mut self, secret: EncSecret) {}
+
+    fn serve() {}
+}
